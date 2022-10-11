08b8759c863e2e0690930dd9c6f0511a1d33c04f
Nabiel Alif <nabiel.alif01@gmail.com>
2022-10-11 23:56:45 +0700

    handling google login

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,6 @@
 const { tokenSign, passCompare } = require('../helpers');
 const { User } = require('../models/index');
+const { OAuth2Client } = require('google-auth-library');
 
 class UserController {
   static async login(req, res, next) {
@@ -24,6 +25,9 @@ class UserController {
 
       res.status(200).json({
         access_token,
+        UserId: findUser.id,
+        role: findUser.role,
+        name: findUser.name,
       });
     } catch (err) {
       next(err);
@@ -37,6 +41,7 @@ class UserController {
         email,
         password,
         name,
+        role: 'User',
         fullName,
         address,
       });
@@ -44,6 +49,74 @@ class UserController {
       res.status(201).json({
         id: response.id,
         email: response.email,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async adminRegister(req, res, next) {
+    try {
+      const { email, name, password, fullName, address } = req.body;
+      const response = await User.create({
+        email,
+        password,
+        name,
+        role: 'Admin',
+        fullName,
+        address,
+      });
+
+      res.status(201).json({
+        id: response.id,
+        email: response.email,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async googleLogin(req, res, next) {
+    try {
+      //token from client
+      const { credential } = req.body;
+
+      const client = new OAuth2Client(process.env.CLIENT_ID);
+      const ticket = await client.verifyIdToken({
+        idToken: credential,
+        audience: process.env.CLIENT_ID,
+      });
+
+      const { name: fullName, email, given_name: name } = ticket.getPayload();
+      console.log(fullName, 'full', name, 'given');
+
+      const [findUser] = await User.findOrCreate({
+        where: { email },
+        hooks: false,
+
+        defaults: {
+          name,
+          email,
+          fullName,
+          password: 'wop',
+          role: 'User',
+          address: 'jalan mana?',
+        },
+      });
+
+      const payload = {
+        id: findUser.id,
+        name: findUser.name,
+        email: findUser.email,
+      };
+
+      const access_token = tokenSign(payload);
+
+      res.status(200).json({
+        access_token,
+        userId: findUser.id,
+        role: findUser.role,
+        name: findUser.name,
       });
     } catch (err) {
       next(err);
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const UserController = require('../controllers/user-controller');
 const router = require('express').Router();
 
 router.post('/register', UserController.register);
+router.post('/admin-register', UserController.adminRegister);
 router.post('/login', UserController.login);
+router.post('/google-login', UserController.googleLogin);
 
 module.exports = router;
