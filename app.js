604b7641503878e7f32abfa8864ae47302fea5c0
Farfour07 <dimassds07@gmail.com>
2022-10-11 15:57:27 +0700

    initial setup for routing feature

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const PORT = 3000
-
+const routes = require('./routes')
 
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(routes)
 
-
-app.get('/', (req, res) => {
-    res.send('Uji Coba Hello World!')
-  })
+// app.get('/', (req, res) => {
+//     res.send('Uji Coba Hello World!')
+//   })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+
+
+
+
+
+
+
+router.get('/', (req, res) => {
+    res.send('Uji Coba 2 Hello World!')
+})
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
+
