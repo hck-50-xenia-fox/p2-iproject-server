c5406444f4f82bf838722a05149e1247085b6ce1
Farfour07 <dimassds07@gmail.com>
2022-10-11 15:50:57 +0700

    setup app done

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
-
+const cors = require('cors')
 const express = require('express')
 const app = express()
+const PORT = 3000
 
 
-
-
+app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Uji Coba Hello World!')
+  })
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
