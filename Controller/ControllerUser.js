793cbd067c3c618f76d2363b74ea35c1b3fede23
Farfour07 <dimassds07@gmail.com>
2022-10-11 16:11:50 +0700

    add jwt helper

--- Controller/ControllerUser.js
+++ Controller/ControllerUser.js
@@ -1,7 +1,26 @@
+const { hashPassword, compareHash } = require("../helpers/bcryptjs")
+const {User} = require("../models")
+
 ///tes tes
+hashPassword
 class ControllerUser{
     static async loginUser(req, res, next) {
-        
+        try {
+            let {email, password} = req.body
+            let data = await User.findOne({
+                where: { email }
+            })
+            if(!data) {
+                console.log(error)
+            }
+            let comparePassword = compareHash(password, data.password)
+            if(!comparePassword) {
+                console.log(error)
+            }
+            
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
