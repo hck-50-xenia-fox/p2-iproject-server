030f365ecabf2a5d8f99a76c2997cfaad7c74e32
mfachrulph21 <mfachrulph21@gmail.com>
2022-10-13 03:33:54 +0700

    insert question post route

---
+++ controllers/questionController.js
@@ -0,0 +1,35 @@
+const { Question } = require('../models/index')
+
+class questionController {
+
+    static async addQuestion (req, res, next) {
+        try {
+            
+            const { description, choice1, choice2, choice3, choice4, answer } = req.body
+
+            if (answer !== choice1 || answer !== choice2 || answer !== choice3 || answer !== choice4 ) {
+                throw {name: 'Not match answer'}
+            }
+
+            await Question.create({
+                description,
+                choice1,
+                choice2,
+                choice3,
+                choice4,
+                answer
+            })
+
+            res.status(201).json({message: `Your question has been accommodated, thank you for your participation.`})
+
+        } catch (error) {
+            if(error.name === 'SequelizeValidationError') {
+                res.status(400).json({message: error.errors[0].message})
+            } else if (error.name === 'Not match answer') {
+                res.status(400).json({message: 'The answers that are filled out do not match the choices given'})
+            } else {
+                res.status(500).jsom({message: 'Internal Server Error'})
+            }
+        }
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const indexController = require('../controllers/indexController');
 const authentication = require('../middleware/authentication');
 const router = express.Router();
 const pokemonRouter = require('./pokemon')
+const questionRouter = require('./question')
 
 router.post('/register', indexController.register);
 router.post('/login', indexController.login);
@@ -10,5 +11,6 @@ router.post('/login', indexController.login);
 router.use(authentication)
 router.post('/register', indexController.updateProfile)
 router.use('/pokemons', pokemonRouter)
+router.use('/questions', questionRouter)
 
 module.exports = router
