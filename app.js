c160749b8fbd4a917b2548c8defe6137fca55c68
adibwafi <adibwafi@gmail.com>
2022-10-12 10:17:22 +0700

    done setup connection to mongo atlas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const routes = require("./routes/index");
 
 const PORT = 3000;
@@ -9,10 +10,19 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+mongoose.connect(
+  "mongodb+srv://adibwafi:Aksesbebas123@sewamotor.38vyhzk.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
 app.use("/", routes);
 
-app.listen(PORT, () => {
-  console.log("terhubung di:", PORT);
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  app.listen(PORT, () => {
+    console.log(`starting on port:`, PORT);
+  });
 });
 
 module.exports = app;
