4c5990fe81a59f84257f4373d578dbf31deb5808
MukhamadTaufikurRachman <m.taufikur.95@gmail.com>
2022-10-13 00:18:05 +0700

    nodemailer and socekt io for server

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const app = express();
 const port = 3000;
 const router = require("./routes/index");
 
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
@@ -16,6 +26,21 @@ app.use(cors());
 app.use("/", router);
 app.use(errorHandler);
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log(socket);
+
+  console.log(`user ${socket.id} is connected`);
+
+  socket.on("message", (data) => {
+    console.log(data, "<< di dalem socket on");
+    socket.broadcast.emit("message:received", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`user ${socket.id} left`);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
