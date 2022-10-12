756bd516e3d9ecb8d51706304fc38949957a14b9
mfachrulph21 <mfachrulph21@gmail.com>
2022-10-13 03:42:41 +0700

    fixing more bug, route GET to POST

--- routes/question.js
+++ routes/question.js
@@ -3,6 +3,6 @@ const questionController = require('../controllers/questionController');
 const router = express.Router();
 
 
-router.get('/', questionController.addQuestion)
+router.post('/', questionController.addQuestion)
 
 module.exports = router
