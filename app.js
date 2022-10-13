599b0f5e43da02c446078506a0eea0d598cf24ab
adibwafi <adibwafi@gmail.com>
2022-10-13 10:53:42 +0700

    deploying to heroku

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-require('dotenv').config()
+require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const routes = require("./routes/index");
-const errorhandler = require('./middlewares/errorhandler')
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-  }
+const errorhandler = require("./middlewares/errorhandler");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-mongoose.connect( process.env.DB_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use("/", routes);
-app.use(errorhandler)
+app.use(errorhandler);
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
