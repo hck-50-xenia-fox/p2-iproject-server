4d0fa7dc0ed4969a05dbc5d38087c1ecf02c07e0
gioiaswndr <annashkagioia24@gmail.com>
2022-10-13 10:23:38 +0700

    add route profile and chess stats

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ router.post('/register', MasterController.register)
 
 router.post('/login', MasterController.login)
 
+router.get('/chessProfile', authen, MasterController.fetchChessProfile)
 
+router.get('/chessStats', authen, MasterController.fetchChessStats)
 
 
 
