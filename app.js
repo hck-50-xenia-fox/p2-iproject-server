f6c056b2f7748d78f163e29aa5ab7c837f073706
MukhamadTaufikurRachman <m.taufikur.95@gmail.com>
2022-10-13 10:51:04 +0700

    revise port

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require("./routes/index");
 
 const http = require("http");
